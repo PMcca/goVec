45d05d496cd53da4334431856d92e8a73e7755d1
Peter McCarthy <peter.mccarthy@openmarket.com>
2019-10-11 14:38:53 +0100

    TODO: Export vector

--- goVec.go
+++ goVec.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
-	"goVec/vector"
-	vec "goVec/vector"
 )
 
-func getVec() vector.Vector3 {
-	v := vector.Vector3{4, 5, 6}
-	println(v.Dot(vec.Vector3{1, 2, 3}))
-	return v
-}
+// func getVec() vector.Vector3 {
+// 	v := vector.Vector3{4, 5, 6}
+// 	println(v.Dot(vec.Vector3{1, 2, 3}))
+// 	return v
+// }
 func main() {
 	// v2 := vector.Vector2{2, 3}
 	// mag := v2.Magnitude()
