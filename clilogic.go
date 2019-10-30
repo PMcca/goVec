333bbee86f47c4e5e0b9f018ae3b129ded7bff20
Peter McCarthy <peter.mccarthy@openmarket.com>
2019-10-30 16:56:15 +0000

    Fix generic parsing

--- clilogic.go
+++ clilogic.go
@@ -13,7 +13,7 @@ func cliActions(c *cli.Context) error {
 	args := c.Args()
 	var vecs2 []v.Vector2
 	var vecs3 []v.Vector3
-	//vecs := make([]v.Vector, 2)
+	vecs := make([]v.Vector, 0)
 
 	if len(args) == 4 { // 2D vectors
 		vecs2 = parseArgs2(args)
@@ -46,16 +46,28 @@ func cliActions(c *cli.Context) error {
 	return nil
 }
 
-func parseArgs2(arg []string) []v.Vector2 {
-	x := parseFloat(arg[0])
-	y := parseFloat(arg[1])
-	v1 := v.NewVector2(x, y)
+func parseArgs(arg []string) []v.Vector {
+	vecs := make([]v.Vector, 2)
+	if len(arg) == 4 {
+		vecs2 := parseArgs2(arg)
+		for i, vec := range vecs2 {
+
+		}
+	}
+	return vecs
+}
+
+func parseArgs2(arg []string, vecs *[]v.Vector) {
+	for i := 0; i < len(arg); i += 2 {
+		x := parseFloat(arg[i])
+		y := parseFloat(arg[i+1])
+		v := v.NewVector2(x, y)
+		vecs = append(vecs, v)
+	}
 
 	x = parseFloat(arg[2])
 	y = parseFloat(arg[3])
 	v2 := v.NewVector2(x, y)
-
-	return []v.Vector2{v1, v2}
 }
 
 func parseArgs3(arg []string) []v.Vector3 {
